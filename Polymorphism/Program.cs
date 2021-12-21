59c6e2878ffd8e07cdee1f76ef67f4925f1dc765
Sahia123 <sahiatech123@gmail.com>
2021-12-21 09:38:42 +0500

    This is my 1st commit

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        // Method Overloading
+        class Student
+        {
+            public virtual void payfees()
+            {
+                Console.WriteLine("Students pay fees");
+            }
+            public void payfees(string a)
+            {
+                Console.WriteLine("Student " +a+ " pay fees");
+            }
+            public void payfees(string c , float b)
+            {
+                Console.WriteLine("Student " + c +  " pay fees Rs:" + b);
+            }
+
+        }
+        // overriding
+        class Exam : Student
+        {
+            public override void payfees()
+            {
+                Console.WriteLine("Only those who pay fees will give exam");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.payfees();
+            s.payfees("Sahia");
+            s.payfees("Saeed" , 6500.5F);
+            Exam E = new Exam();
+            E.payfees();
+            
+        }
+    }
+}
+//overloading depends upon type of parameters and no. of parameters
